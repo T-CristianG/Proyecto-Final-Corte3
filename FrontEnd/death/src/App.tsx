b75a25e68a4260dd1691589560562915a4baaafa
T-CristianG <cristianc.torog@gmail.com>
2025-05-01 18:22:37 -0500

    typescript

--- FrontEnd/death/src/App.tsx
+++ FrontEnd/death/src/App.tsx
@@ -1,26 +1,18 @@
 import React from "react";
 
+import './App.css';
+
 const App: React.FC = () => {
   return (
-    <div style={{
-      width: "100vw",
-      height: "100vh",
-      backgroundColor: "#f4e0c5",
-      border: "5px solid #964b00",
-      padding: "40px",
-      boxSizing: "border-box",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center"
-    }}>
-      <h1 style={{ textAlign: "center", fontSize: "3rem" }}>El Gran Viaje</h1>
-      <p style={{ fontSize: "1.5rem", textAlign: "justify", maxWidth: "80%" }}>
-        En un mundo desconocido, un aventurero busca respuestas que cambiarán su destino para siempre...
-      </p>
+    <div className="container">
+      <div className="front-cover">
+        <img src="../../images/death-note.png" alt="Death Note Logo" />
+      </div>
+      <div className="content" contentEditable={true}></div>
     </div>
   );
 };
+
 
 export default App;
 
